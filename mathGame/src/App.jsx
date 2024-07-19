4186946ac549141729a8963184b4d2d15a259ec4
W. Brad Meyer <wbradmeyer@gmail.com>
2024-07-19 11:21:13 -0400

    create select comp

--- mathGame/src/App.jsx
+++ mathGame/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import MultProblem from "./components/MultProblem";
+import Select from "./components/Select";
 
 function App() {
+  const [operator, setOperator] = useState("");
+
   return (
     <>
+      <Select></Select>
       <MultProblem></MultProblem>
     </>
   );
---
+++ mathGame/src/components/Select.jsx
@@ -0,0 +1,9 @@
+import { React, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+
+const Select = () => {
+  return <div>Select</div>;
+};
+
+export default Select;
