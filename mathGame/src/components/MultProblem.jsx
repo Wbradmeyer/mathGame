4e6a4f1f390ef37180b00cbed14a2efcb9559eb3
W. Brad Meyer <wbradmeyer@gmail.com>
2024-07-19 17:07:51 -0400

    1st try plus, min, mult

--- mathGame/src/components/MultProblem.jsx
+++ mathGame/src/components/MultProblem.jsx
@@ -2,40 +2,65 @@ import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 
-const MultProblem = () => {
+const MultProblem = (props) => {
+  const { operator, setOperator } = props;
   let [level, setLevel] = useState(1);
-  const [answer, setAnswer] = useState("");
+  // const [answer, setAnswer] = useState("");
+  const [answer, setAnswer] = useState(0);
   const [factor1, setFactor1] = useState(0);
   const [factor2, setFactor2] = useState(0);
+  const [operand1, setOperand1] = useState(0);
+  const [operand2, setOperand2] = useState(0);
+  const [calculation, setCalculation] = useState(0);
   let product = factor1 * factor2;
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    setFactor1(Math.floor(Math.random() * 10 + 1));
-    setFactor2(Math.floor(Math.random() * 10 + 1));
+    // setFactor1(Math.floor(Math.random() * 10 + 1));
+    // setFactor2(Math.floor(Math.random() * 10 + 1));
+    setOperand1(Math.floor(Math.random() * 10 + 1));
+    setOperand2(Math.floor(Math.random() * 10 + 1));
+    calculate(operand1, operand2);
   }, []);
 
   const handleVals = (e) => {
     setAnswer(e.target.value);
   };
 
+  const calculate = (num1, num2) => {
+    if (operator == "+") setCalculation(num1 + num2);
+    else if (operator == "-") setCalculation(num1 - num2);
+    else if (operator == "*") setCalculation(num1 * num2);
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    if (Number(answer) == product) {
+    // if (Number(answer) == product) {
+    if (answer == calculation) {
       if (level == 10) {
         setMessage("You Win!");
       } else {
         setLevel(level + 1);
-        setAnswer("");
+        // setAnswer("");
+        setAnswer(0);
         if (level == 9) {
-          setFactor1(Math.floor(Math.random() * 10 + 11));
-          setFactor2(Math.floor(Math.random() * 10 + 11));
+          // setFactor1(Math.floor(Math.random() * 10 + 11));
+          // setFactor2(Math.floor(Math.random() * 10 + 11));
+          setOperand1(Math.floor(Math.random() * 10 + 11));
+          setOperand2(Math.floor(Math.random() * 10 + 11));
+          calculate(operand1, operand2);
         } else if (level >= 5) {
-          setFactor1(Math.floor(Math.random() * 10 + 11));
-          setFactor2(Math.floor(Math.random() * 10 + 1));
+          // setFactor1(Math.floor(Math.random() * 10 + 11));
+          // setFactor2(Math.floor(Math.random() * 10 + 1));
+          setOperand1(Math.floor(Math.random() * 10 + 11));
+          setOperand2(Math.floor(Math.random() * 10 + 1));
+          calculate(operand1, operand2);
         } else {
-          setFactor1(Math.floor(Math.random() * 10 + 1));
-          setFactor2(Math.floor(Math.random() * 10 + 1));
+          // setFactor1(Math.floor(Math.random() * 10 + 1));
+          // setFactor2(Math.floor(Math.random() * 10 + 1));
+          setOperand1(Math.floor(Math.random() * 10 + 1));
+          setOperand2(Math.floor(Math.random() * 10 + 1));
+          calculate(operand1, operand2);
         }
       }
     }
@@ -50,10 +75,15 @@ const MultProblem = () => {
         <div>
           {message ? <h1>{message}</h1> : <h1>Level {level}</h1>}
           <div className="problemBox">
-            <p className="number">{factor1}</p>
+            {/* <p className="number">{factor1}</p>
             <div className="operation">
               <p className="number">X</p>
               <p className="number">{factor2}</p>
+            </div> */}
+            <p className="number">{operand1}</p>
+            <div className="operation">
+              <p className="number">{operator}</p>
+              <p className="number">{operand2}</p>
             </div>
           </div>
           <hr />
